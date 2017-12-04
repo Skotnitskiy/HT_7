7ff95dd50edc419fd6df91a3bde6218222a329d3
serhSK <sergeyskotnitskiy@gmail.com>
2017-12-05 00:58:05 +0200

    added calculator

--- ht7.py
+++ ht7.py
@@ -33,3 +33,56 @@ class Credit(object):
 
 print("Must returned", Credit.sum())
 print("Must returned per month", Credit.amount_per_month())
+
+
+class Calculator(object):
+    results = []
+
+    @staticmethod
+    def add(a, b):
+        Calculator.results.append({str(a) + " + " + str(b): a + b})
+
+    @staticmethod
+    def subtract(a, b):
+        Calculator.results.append({str(a) + " - " + str(b): a - b})
+
+    @staticmethod
+    def multiply(a, b):
+        Calculator.results.append({str(a) + " * " + str(b): a * b})
+
+    @staticmethod
+    def divide(a, b):
+        try:
+            Calculator.results.append({str(a) + " / " + str(b): a / b})
+        except ZeroDivisionError:
+            Calculator.results.append({str(a) + " / " + str(b): "Zero division error!"})
+            print("Zero division error!")
+
+    @staticmethod
+    def get_all_results():
+        for result in Calculator.results:
+            for operation, res in zip(result.keys(), result.values()):
+                print(operation, "=", res)
+
+    @staticmethod
+    def get_results(amount):
+        if amount < len(Calculator.results):
+            for i in range(amount):
+                result = Calculator.results[i]
+                for operation, res in zip(result.keys(), result.values()):
+                    print(operation, "=", res)
+
+    @staticmethod
+    def get_previous_result():
+        result = Calculator.results[len(Calculator.results) - 2]
+        for operation, res in zip(result.keys(), result.values()):
+            print(operation, "=", res)
+
+
+Calculator.add(3, 2)
+Calculator.subtract(3, 2)
+Calculator.multiply(3, 7)
+Calculator.divide(7, 0)
+Calculator.get_all_results()
+Calculator.get_previous_result()
+Calculator.get_results(2)
